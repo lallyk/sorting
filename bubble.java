9915522f2456d097462cf33b68d8202ad8f51fa4
lallyk <114976253+lallyk@users.noreply.github.com>
2022-10-04 11:26:22 +0530

    sorting an object array

--- bubble.java
+++ bubble.java
@@ -20,3 +20,46 @@ class HelloWorld {
         }
     }
 }
+//sorting objets using bubble sort
+import java.util.*;
+class Employee implements comparable<Employee>{
+    String Name;
+    long salary;
+    Employee(String N,long s)
+    {
+        Name=N;
+        salary=s;
+    }
+    public void show()
+    {
+        System.out.println(Name );
+    }
+}
+class Main
+{
+    public static void main(String[] args) {
+        Employee e[]=new Employee[5];
+        e[0]=new Employee("sai",30000);
+        e[1]=new Employee("ram",40000);
+        e[2]=new Employee("lally",20000);
+        e[3]=new Employee("deekshu",50000);
+        e[4]=new Employee("krishna",30000);
+        Employee temp;
+        for (int j = 0; j <e.length; j++)
+        {
+            for (int i = j + 1; i <e.length; i++)
+            {
+                if (e[j].compareTo(e[i]) > 0)
+                {
+                    temp = e[j];
+                    e[j] = e[i];
+                    e[i] = temp;
+                }
+            }
+        }
+        for(int i=0;i<e.length;i++)
+        {
+           e[i].show();
+        }
+    }
+}
